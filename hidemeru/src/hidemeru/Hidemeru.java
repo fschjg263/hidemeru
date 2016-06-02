df970c686847e6cc3ef92f19058f5f2573f41fa9
fschjg263 <alexander.mueller187@gmail.com>
2016-06-02 23:19:11 +0200

    Hidemeru.java erweitert

--- hidemeru/src/hidemeru/Hidemeru.java
+++ hidemeru/src/hidemeru/Hidemeru.java
@@ -5,17 +5,43 @@
  */
 package hidemeru;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.util.List;
+
 /**
  *
  * @author am
  */
 public class Hidemeru {
 
+    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        List<String> vpnlist = null;
+        String[] tmpList;
+        if (args.length < 1) {
+            System.out.println("Eine Datei angeben!");
+            System.exit(1);
+        }
+        File dir = new File(args[0]);
+        if (!dir.exists()) {
+            System.out.println(dir.getName()+" existiert nicht!");
+        } else if (!dir.isDirectory()) {
+            System.out.println(dir.getName() + " ist kein Ordner!");
+            System.exit(1);
+        }
+        tmpList = dir.list();
+        for (int i=0; i < tmpList.length; i++){
+            if(tmpList[i].endsWith(".ovpn")){
+                vpnlist.add(tmpList[i]);
+            }
+        }
+        System.out.println("This files were found: "+vpnlist.toString());        
+
     }
-    
+
 }
